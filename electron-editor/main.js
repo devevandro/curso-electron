bcd02d5a92ce087fadd448202f119cf00546e658
Evandro Carvalho Ferreira <vandin23cedrick@gmail.com>
2020-05-25 22:23:07 -0300

    modificando o tamanho do texto

--- electron-editor/main.js
+++ electron-editor/main.js
@@ -1,5 +1,5 @@
 const electron = require('electron');
-const { app,  BrowserWindow } = electron;
+const {app,  BrowserWindow} = electron;
 
 app.on('ready', () => {
     let win = new BrowserWindow({});
